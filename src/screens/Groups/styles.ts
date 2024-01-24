5fa31d167dcc9db5f35d5dee2a96dcdc87447154
Georgi Santana <georgi.andersson@hotmail.com>
2024-01-24 19:51:18 -0300

    Implementação utilizando styled-components

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #000;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    
+    
+    
+`;
+
+export const Title = styled.Text`
+    
+    color: #FFF;
+    font-size: 36px;
+    
+    
+
+    
+    
+    
+    
+`;
+
+
