dbb1787c43b7fd11e9cee906f0bf9497c931ab54
Georgi Santana <georgi.andersson@hotmail.com>
2024-02-28 20:28:53 -0300

    Criado arquivo de components e implemetado o component de Loading

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { Groups } from "./src/screens/Groups";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
 import {useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
-import {ActivityIndicator} from "react-native"
+import { Loading } from "./src/components/Loading";
 
 
 export default function App() {
@@ -10,7 +10,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-   { fontsLoaded ? <Groups/> : <ActivityIndicator/>}
+   { !fontsLoaded ? <Groups/> : <Loading/>}
    </ThemeProvider>
   );
 }
